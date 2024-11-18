2129255fdeb9429d472135f3d7378135a57ab773
Rimadebnath123 <rimacse5@gmail.com>
2024-11-18 21:02:38 +0530

    AddBootstrap and new functionality

--- TextUtils/TextUtils/views.py
+++ TextUtils/TextUtils/views.py
@@ -8,7 +8,11 @@ def analyze(request):
     #Get the text
     djtext = request.GET.get('text', 'default')
     removepunc = request.GET.get('removepunc', 'off')
-    print(djtext)
+    fullcaps=request.GET.get('fullcaps','off')
+    newlineremover=request.GET.get('newlineremover','off')
+    extraspaceremover=request.GET.get('extraspaceremover','off')
+    charCounter=request.GET.get('charCounter','off')
+
     if removepunc=="on":
         punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
         analyzed = ""
@@ -16,6 +20,50 @@ def analyze(request):
             if char not in punctuations:
                 analyzed = analyzed + char
         params = {'purpose': 'Removed Punctuations', 'analyzed_text': analyzed}
+        return render(request, 'analyze.html', params)
+    elif fullcaps=="on":
+        analyzed=""
+        for char in djtext:
+            analyzed=analyzed+char.upper()
+        params = {'purpose': 'Change To Uppercase', 'analyzed_text': analyzed}
+        return render(request, 'analyze.html', params)
+    elif newlineremover=="on":
+        analyzed=""
+        for char in djtext:
+            if char!="\n":
+                analyzed=analyzed+char
+        params = {'purpose': 'Removed NewLines', 'analyzed_text': analyzed}
+
+    # Analyze the text
+
+        return render(request, 'analyze.html', params)
+    elif(extraspaceremover=="on"):
+        analyzed = ""
+        for index, char in enumerate(djtext):
+            if not(djtext[index] == " " and djtext[index+1]==" "):
+                analyzed = analyzed + char
+
+        params = {'purpose': 'Removed ExtraSpace', 'analyzed_text': analyzed}
+
+        # Analyze the text
+        return render(request, 'analyze.html', params)
+    elif(charCounter=="on"):
+        analyzed = ""
+        char_count = 0 
+
+        for char in djtext:
+            if char != " ":  # Exclude spaces from the character count
+                char_count += 1
+                analyzed += char  
+
+    # Prepare the parameters for rendering
+        params = {
+            'purpose': 'Character Counter',
+            'analyzed_text': analyzed,
+            'char_count': char_count,  
+            }
+
+        # Analyze the text
         return render(request, 'analyze.html', params)
     else:
         return HttpResponse('Error')
